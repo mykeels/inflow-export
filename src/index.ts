ed3542d5cf135a6dd1167ecd75197c6c336492fd
mykeels <mykehell123@gmail.com>
2021-09-12 16:47:27 +0100

    chore: use try-catch

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,28 @@ import { saveToCSVFile } from "./save-transactions";
 
 if (require.main === module) {
   (async () => {
-    exportInflowTransactions(
-      getAccessTokenFromConsole,
-      async (accessToken: string) => {
-        const { startDate, endDate } = await getDateRangeFromConsole();
-        return getTransactionsInDateRange(accessToken, {
-          startDate,
-          endDate,
-        });
-      },
-      transformInflowTransactionsToCSV,
-      saveToCSVFile
-    );
+    try {
+      await exportInflowTransactions(
+        getAccessTokenFromConsole,
+        async (accessToken: string) => {
+          const { startDate, endDate } = await getDateRangeFromConsole();
+          return getTransactionsInDateRange(accessToken, {
+            startDate,
+            endDate,
+          });
+        },
+        transformInflowTransactionsToCSV,
+        saveToCSVFile
+      );
+      process.exit(0);
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
   })();
 }
+
+export * from "./core";
+export * from "./core/models";
+export * from "./get-transactions";
+export * from "./transform-transactions";
